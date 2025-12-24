f089e63fd5293bddde806f898e466da5fe794482
Christopher Sharkey <ev2900@gmail.com>
2025-12-24 10:23:34 -0500

    Update client.py

--- STDIO/client.py
+++ STDIO/client.py
@@ -6,13 +6,25 @@ async def main():
 	# Create an MCP client that will START the server as a subprocess using: `python server.py`
     client = Client(StdioTransport(command="python", args=["server.py"]))
 
-    # The client will communicate with that subprocess mcp server via stdin/stdout
+    # The client will communicate with the already running mcp server via HTTP
     async with client:
-    	# Call the MCP tool named "hello", arguments must match the tool's schema
-        result = await client.call_tool("hello", {"name": "world"})
-        
+        # Get a list of all the tools available via. the MCP server
+        result_1 = await client.list_tools()
+
+        # Print the name and description of each tool
+        print('-- Tools --')
+        for tool in result_1:
+            # print(tool)
+            print(f'Name: {tool.name}')
+            print(f'Description: {tool.description}')
+
+        # Call the MCP tool named "hello", arguments must match the tool's schema
+        result_2 = await client.call_tool("hello", {"name": "world"})
+
         # Print the tool's return value
-        print(result)
+        print('-- Result from calling hello tool --')
+        # print(result_2)
+        print(result_2.content[0].text)
 
 # Required because MCP client APIs are async
 asyncio.run(main())
