d5582976c7d5cfc89471c6fe84dc9db7839eea52
Christopher Sharkey <ev2900@gmail.com>
2025-12-24 10:18:33 -0500

    Update client.py

--- HTTP/client.py
+++ HTTP/client.py
@@ -7,11 +7,23 @@ async def main():
 
     # The client will communicate with the already running mcp server via HTTP
     async with client:
+        # Get a list of all the tools available via. the MCP server
+        result_1 = await client.list_tools()
+
+        # Print the name and description of each tool
+        print('-- Tools --')
+        for tool in result_1:
+            # print(tool)
+            print(f'Name: {tool.name}')
+            print(f'Description: {tool.description}')
+
         # Call the MCP tool named "hello", arguments must match the tool's schema
-        result = await client.call_tool("hello", {"name": "world"})
+        result_2 = await client.call_tool("hello", {"name": "world"})
 
         # Print the tool's return value
-        print(result)
+        print('-- Result from calling hello tool --')
+        # print(result_2)
+        print(result_2.content[0].text)
 
 # Required because MCP client APIs are async
 asyncio.run(main())
